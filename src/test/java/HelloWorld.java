1a9c6c1f55b5294ad221f5e8eb4374881b8a53e7
serhiiivashchenko <sergiy.ivaschenko@gmail.com>
2015-03-22 21:06:12 +0200

    bboysh1ft test commit

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -4,6 +4,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.print("Test changes from remote");
+        System.out.print("More Test changes from remote");
     }
 }
